ce15087c8c4d5d5a4c493387d6f6b487af8450fa
Anton <anton.t@notpx.app>
2026-05-25 15:34:50 +0300

    new release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3806075731" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1129085538" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
---
+++ google_maps_env.js
@@ -0,0 +1,4 @@
+window.__APP_CONFIG__ = {
+  ...(window.__APP_CONFIG__ || {}),
+  GOOGLE_MAPS_API_KEY_WEB: "AIzaSyBH2PJ1mnzVqWHdkosFlaqugSk-pNhCGss"
+};
