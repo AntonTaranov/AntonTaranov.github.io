53020867fd99cbd6097286d1e8d61cfe3c412a07
Anton <anton.t@notpx.app>
2026-05-31 16:31:44 +0300

    update auth flow

--- google_maps_env.js
+++ google_maps_env.js
@@ -1,4 +1,5 @@
 window.__APP_CONFIG__ = {
   ...(window.__APP_CONFIG__ || {}),
-  GOOGLE_MAPS_API_KEY_WEB: "AIzaSyBH2PJ1mnzVqWHdkosFlaqugSk-pNhCGss"
+  GOOGLE_MAPS_API_KEY_WEB: "AIzaSyBH2PJ1mnzVqWHdkosFlaqugSk-pNhCGss",
+  GOOGLE_MAPS_WEB_MAP_ID: "f77dcaf96fe601e65f48dff9"
 };
